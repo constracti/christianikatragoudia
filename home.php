31be925b30ce4d86cc0f2fcca0a349a6cde348e0
constracti <constracti@gmail.com>
2025-12-31 02:32:01 +0200

    remove elementor

--- home.php
+++ home.php
@@ -5,7 +5,7 @@ if ( !defined( 'ABSPATH' ) )
 
 add_action( 'pre_get_posts', function( WP_Query $query ): void {
 	global $wp_the_query;
-	if ( !$wp_the_query->is_page( 3260 ) )
+	if ( !$wp_the_query->is_page( 9190 ) )
 		return;
 	if ( $query->get( 'post_type' ) !== 'post' )
 		return;
@@ -20,66 +20,29 @@ add_action( 'pre_get_posts', function( WP_Query $query ): void {
 
 add_action( 'pre_get_posts', function( WP_Query $query ): void {
 	global $wp_the_query;
-	if ( !$wp_the_query->is_page( 3260 ) )
+	if ( !$wp_the_query->is_page( 9190 ) )
 		return;
 	if ( $query->get( 'post_type' ) !== 'post' )
 		return;
-	if ( $query->get( 'orderby' ) !== 'rand' )
+	if ( $query->get( 'orderby' ) !== 'rand' && $query->get( 'orderby' ) !== 'title' )
 		return;
 	if ( $query->get( 'order' ) !== 'ASC' )
 		return;
 	if ( $query->get( 'posts_per_page' ) !== 4 )
 		return;
+	$query->set( 'orderby', 'rand' );
 	$query->set( 'posts_per_page', 12 );
 } );
 
-add_action( 'the_content', function( string $content ): string {
-	global $wp_the_query;
-	if ( !$wp_the_query->is_page( 3260 ) )
-		return $content;
+add_shortcode( 'xt_count', function( array $atts ): string {
+	if ( !isset( $atts['category_name'] ) )
+		return '';
+	if ( !is_string( $atts['category_name'] ) )
+		return '';
 	$count = count( get_posts( [
+		'category_name' => $atts['category_name'],
 		'nopaging' => TRUE,
 		'fields' => 'ids',
 	] ) );
-	foreach ( [ 1, 2, 3, 4, ] as $case ) {
-		$count = NULL;
-		$args = NULL;
-		switch ( $case ) {
-			case 1:
-				$args = [
-					'category_name' => 'songs',
-				];
-				break;
-			case 2:
-				$args = [
-					'post_type' => 'attachment',
-					'post_mime_type' => 'text/plain',
-					's' => '.chords',
-				];
-				break;
-			case 3:
-				$args = [
-					'post_type' => 'attachment',
-					'post_mime_type' => 'application/pdf',
-					's' => '-full',
-				];
-				break;
-			case 4:
-				$args = [
-					'post_type' => 'attachment',
-					'post_mime_type' => 'audio/mpeg',
-					's' => '.featured',
-				];
-				break;
-		}
-		if ( !is_null( $args ) ) {
-			$args['nopaging'] = TRUE;
-			$args['fields'] = 'ids';
-			$count = count( get_posts( $args ) );
-			$pattern = '<div class="het-counter-count odometer" data-count="' . $case . '">';
-			$replace = '<div class="het-counter-count odometer" data-count="' . $count . '">';
-			$content = mb_ereg_replace( $pattern, $replace, $content );
-		}
-	}
-	return $content;
+	return strval( $count );
 } );
